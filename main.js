7921c7c786a561fad2e495d199daebd2e1bef20a
Chernoff_Kirill <chernoff.kirill@gmail.com>
2015-02-03 22:31:36 +0600

    + доделал алгоритм стрельбы компьютера. Теперь ПК значительно поумнел при обстреле кораблей. Ищется направление корабля и ведется обстрел в этом направлении. Алгоритм не учитывает первое попадание например в центр 4х палубного корабля. В таком случае ПК может уничтожить только половину корабля, а оставшуюся добьет в следующий раз, если попадет на еще не уничтоженную палубу. К тому же алгоритм не учитывает поле вокруг корабля. поэтому возможно попадания мимо рядом с уже уничтоженным кораблем. Данную стратегию можно списать на то, что пользователь не знает в скольки палубный корабль он попал и ему приходится делать на 1-2 хода больше до полного уничтожения корабля.

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ function init(){
 		    aiMap[i][j] = '~';
 		}
 	}
+
+    createField();
 }
 
 function createField(){
@@ -38,8 +40,7 @@ function createField(){
             divAi.id = i + '_' + j, divAi.className = aiMap[i][j] == 'deck' ? 'deck' : 'sea';
 			divAi.onclick = function() {
 			    if (playerShoot(this)) {
-			        aiMove();
-			        //добавить функцию, при которой ПК будет открывать ответный огонь
+			        aiGame();
 			    }
 			}
 			aiField.appendChild(divAi);
@@ -172,35 +173,12 @@ function playerShoot(elem) {
     return false;
 }
 
-function aiMove() {
-
-    var sX, sY;
-
-    if (document.querySelectorAll('#playerField .deck').length === 0) {
-        alert("You lose!");
-    } else {
-
-        do {
-            sX = getRandomInt(0, pole - 1);
-            sY = getRandomInt(0, pole - 1);
-        } while (!checkCellToFire(sX, sY));
-
-        aiShoot(sX, sY);
-    }
-}
-
 function aiShoot(dX, dY) {
-    
+
     var element = document.getElementById(dX + '_' + dY);
 
     if (element.className == 'deck') {
         element.className = 'hit';
-
-        var ddX = getRandomInt(0, pole - 1);
-        var ddY = getRandomInt(0, pole - 1);
-
-        aiShoot(ddX, ddY);
-
         return true;
     } else {
         if (element.className == 'sea') {
@@ -210,6 +188,7 @@ function aiShoot(dX, dY) {
     }
 }
 
+//функция проверяет клетку на наличие выстрела по ней
 function checkCellToFire(cX, cY) {
     var element = document.getElementById(cX + '_' + cY);
 
@@ -218,4 +197,93 @@ function checkCellToFire(cX, cY) {
     } else {
         return true;
     }
+}
+
+var aiState = 0;
+var memX, memY;
+
+function aiGame() {
+
+    var sX, sY;
+
+    switch (aiState) {
+        case 0:
+            do {
+                sX = getRandomInt(0, pole - 1);
+                sY = getRandomInt(0, pole - 1);
+            } while (!checkCellToFire(sX, sY));
+
+            if (aiShoot(sX,sY)) {
+                memX = sX;
+                memY = sY;
+                aiState = 1;
+                lastIndex = 0;
+                aiGame();
+            }
+            break;
+        case 1:
+            if (findDeckDirection(memX, memY)) {
+                aiState = 2;
+                aiGame();
+            }
+            break;
+
+        case 2:
+            aiFire();
+            break;
+    }
+}
+
+var lastIndex;
+var findArr = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+var foundDirection;
+
+function findDeckDirection(fX, fY) {
+    for (var i = lastIndex; i < findArr.length; i++) {
+        lastIndex = i;
+
+        var tempX = fX + findArr[i][0];
+        var tempY = fY + findArr[i][1];
+
+        if (cellAbroadField(tempX, tempY) || !checkCellToFire(tempX, tempY)) {
+            continue;
+        } else {
+            if (!aiShoot(tempX, tempY)) {
+                return false;
+            } else {
+                foundDirection = i;
+
+                memX = tempX;
+                memY = tempY;
+
+                return true;
+            }
+        }
+    }
+
+    if ((lastIndex == findArr.length - 1) && aiState == 1) {
+        aiState = 0;
+    }
+}
+
+function aiFire() {
+
+    if (document.querySelectorAll('#playerField .deck').length === 0) {
+        alert("You lost!");
+    } else {
+
+        do {
+            memX = memX + findArr[foundDirection][0];
+            memY = memY + findArr[foundDirection][1];
+
+            //console.log("memX: ", memX, "memY: ", memY);
+        } while (!cellAbroadField(memX, memY) && aiShoot(memX, memY))
+
+    }
+    aiState = 0;
+}
+
+function cellAbroadField(abX, abY) {
+    if (abX < 0 || abX > 9 || abY < 0 || abY > 9) return true;
+    else return false;
 }
